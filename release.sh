0048ac868614db94cd14997c44baa9536aa0fec7
Obfuscoder <obfuscoder@obfusco.de>
2015-09-19 10:10:49 +0200

    Bump version to 3.0.2

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
-set version=$1
-mvn versions:set -DnewVersion=$version
+VERSION=$1
+mvn versions:set -DnewVersion=$VERSION
 mvn clean package -DskipTests
 cp flohmarkt.iss.template flohmarkt.iss
-sed -i -- "s/##VERSION##/$version/g" flohmarkt.iss
+sed -i -- "s/##VERSION##/$VERSION/g" flohmarkt.iss
 wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Inno\ Setup\ 5/ISCC.exe flohmarkt.iss
-gcam "Bump version to $version"
-gpa
-gpa --tags
+git commit -a -m "Bump version to $VERSION"
+git remote | xargs -L1 git push
+gpa remote | xargs -L1 git push --tags
 scp Output/flohmarkthelfer-setup.exe flohmarkthelfer:html/download
 
