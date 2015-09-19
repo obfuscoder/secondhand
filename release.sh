103a2aacd904e09ba82f8604355173198295c72d
Obfuscoder <obfuscoder@obfusco.de>
2015-09-19 10:13:21 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
 VERSION=$1
 mvn versions:set -DnewVersion=$VERSION
+rm **/*.versionsBackup
 mvn clean package -DskipTests
 cp flohmarkt.iss.template flohmarkt.iss
 sed -i -- "s/##VERSION##/$VERSION/g" flohmarkt.iss
 wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Inno\ Setup\ 5/ISCC.exe flohmarkt.iss
 git commit -a -m "Bump version to $VERSION"
+git tag $VERSION
 git remote | xargs -L1 git push
 gpa remote | xargs -L1 git push --tags
 scp Output/flohmarkthelfer-setup.exe flohmarkthelfer:html/download
