e092871e8f58022e9ec971f778725fbd3ee01596
Obfuscoder <obfuscoder@obfusco.de>
2016-01-10 12:54:38 +0100

    Update label creation to fit on stickers and cope with longer texts
    Close #17

--- secondhand-labelgenerator/src/main/java/de/obfusco/secondhand/labelgenerator/LabelSheet.java
+++ secondhand-labelgenerator/src/main/java/de/obfusco/secondhand/labelgenerator/LabelSheet.java
@@ -30,43 +30,24 @@ public class LabelSheet {
 
     public Path createPdf(Path targetPath) throws DocumentException, IOException {
 
-        Document document = new Document(PageSize.A4, 0, 0,
-                Utilities.millimetersToPoints(20), Utilities.millimetersToPoints(20));
+        Document document = new Document(PageSize.A4,
+                Utilities.millimetersToPoints(3), Utilities.millimetersToPoints(5),
+                Utilities.millimetersToPoints(22), Utilities.millimetersToPoints(22));
         Files.createDirectories(targetPath);
         Path filePath = Paths.get(targetPath.toString(), "labels.pdf");
-        PdfWriter writer = PdfWriter.getInstance(document,
-                new FileOutputStream(filePath.toFile()));
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(filePath.toFile()));
         document.open();
 
-        PdfPTable table = createTableLine();
+        PdfPTable table = createTable();
         for (int i = 0; i < items.size(); i++) {
-            table.addCell(createTableCell(writer, items.get(i)));
-        }
-        for (int i=0; i<(NUMBER_OF_COLUMNS-(items.size() % NUMBER_OF_COLUMNS)) % NUMBER_OF_COLUMNS; i++) {
-            PdfPCell cell = new PdfPCell(new Paragraph(""));
-            cell.setBorderColor(BaseColor.WHITE);
+            PdfPCell cell = createTableCell(writer, items.get(i));
+            cell.setPadding(6);
             table.addCell(cell);
         }
+        table.completeRow();
         document.add(table);
         document.close();
         return filePath;
-    }
-
-    private void drawGrid(PdfWriter writer) {
-        PdfContentByte cb = writer.getDirectContent();
-        cb.saveState();
-        cb.setColorStroke(BaseColor.BLACK);
-        for(int c=0; c<3; c++) {
-            for (int r=0; r<5; r++) {
-                cb.rectangle(
-                        Utilities.millimetersToPoints(c * 70),
-                        Utilities.millimetersToPoints(r*51+20),
-                        Utilities.millimetersToPoints(70),
-                        Utilities.millimetersToPoints(51));
-            }
-        }
-        cb.stroke();
-        cb.restoreState();
     }
 
     public Path createPDFFile(Path basePath, Reservation reservation) throws DocumentException, IOException {
@@ -77,137 +58,114 @@ public class LabelSheet {
         return createPdf(targetPath);
     }
 
-    private Element createCellContent(PdfWriter writer, Item item) {
+    private Element createCellContent(PdfWriter writer, Item item) throws IOException, DocumentException {
         PdfContentByte cb = writer.getDirectContent();
+        PdfPTable table = new PdfPTable(12);
+        table.setWidthPercentage(100);
+
+        PdfPCell cell = createNumberCell(item);
+        cell.setColspan(6);
+        cell.setFixedHeight(getLabelCellHeight(writer) / 11 * 2);
+        table.addCell(cell);
+
+        cell = createPriceCell(item);
+        cell.setColspan(6);
+        cell.setFixedHeight(getLabelCellHeight(writer) / 11 * 2);
+        table.addCell(cell);
+
+        int colSpanForDetails = table.getNumberOfColumns();
+        int colSpanForDonation = 2;
+        if (item.donation) {
+            PdfPCell donationCell = createDonationCell();
+            donationCell.setColspan(colSpanForDonation);
+            table.addCell(donationCell);
+            colSpanForDetails = table.getNumberOfColumns() - colSpanForDonation;
+        }
+
+        cell = createDetailsCell(item);
+        cell.setColspan(colSpanForDetails);
+        cell.setFixedHeight(getLabelCellHeight(writer) / 11 * 5);
+        table.addCell(cell);
 
         Barcode128 barcode = new Barcode128();
         barcode.setCodeType(Barcode.CODE128);
         barcode.setCode(item.code);
 
-        PdfPTable table = new PdfPTable(5);
-        table.setTotalWidth(180);
-        table.setLockedWidth(true);
-
-        PdfPCell cell = createSellerNumberCell(item);
-        table.addCell(cell);
-
-        cell = createItemNumberCell(item);
-        table.addCell(cell);
-
-        cell = createCategoryCell(item);
-        table.addCell(cell);
-
-        if (item.donation) table.addCell(createDonationCell(item, table));
-
-        cell = createDescriptionCell(item, table);
-        table.addCell(cell);
-
-        cell = createSizeCell(item);
-        table.addCell(cell);
-
-        cell = createPriceCell(item);
-        table.addCell(cell);
-
-        cell = new PdfPCell(new Phrase(" "));
-        cell.setColspan(table.getNumberOfColumns());
-        cell.setBorderColor(BaseColor.WHITE);
-        table.addCell(cell);
-
         cell = createBarcodeCell(cb, barcode);
+        cell.setColspan(table.getNumberOfColumns());
+        cell.setFixedHeight(getLabelCellHeight(writer) / 11 * 4);
         table.addCell(cell);
-
-        table.setSpacingAfter(2f);
-
         return table;
     }
 
-    private PdfPCell createDonationCell(Item item, PdfPTable table) {
-        PdfPCell cell = new PdfPCell(new Phrase(new Chunk("S", FontFactory.getFont(FontFactory.HELVETICA, 40))));
-        cell.setColspan(1);
-        cell.setRowspan(3);
-        cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-        cell.setBorderColor(BaseColor.WHITE);
+    private PdfPCell createDonationCell() {
+        PdfPCell cell = new PdfPCell(new Phrase(new Chunk("S", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 30))));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setBorderColor(BaseColor.BLACK);
+        cell.setBorderWidth(1);
         return cell;
     }
 
     private PdfPCell createBarcodeCell(PdfContentByte cb, Barcode128 codeEAN) {
         Image barcode = codeEAN.createImageWithBarcode(cb, null, null);
-        barcode.scalePercent(120f);
         PdfPCell cell = new PdfPCell(barcode);
-        cell.setColspan(5);
         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        cell.setPadding(2);
-        cell.setBorderColor(BaseColor.WHITE);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setBorderColor(BaseColor.BLACK);
+        cell.setBorderWidth(1);
         return cell;
     }
 
     private PdfPCell createPriceCell(Item item) {
         NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.GERMANY);
         String price = currencyFormat.format(item.price);
-        PdfPCell cell = new PdfPCell(new Phrase(price, FontFactory.getFont(FontFactory.HELVETICA_BOLD, 22)));
-        cell.setBorderColor(BaseColor.WHITE);
-        cell.setColspan(5 - (item.donation ? 1 : 0));
+        PdfPCell cell = new PdfPCell(new Phrase(price, FontFactory.getFont(FontFactory.HELVETICA_BOLD, 20)));
+        cell.setBorderColor(BaseColor.BLACK);
+        cell.setBorderWidth(1);
         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         return cell;
     }
 
-    private PdfPCell createSizeCell(Item item) {
-        PdfPCell cell = new PdfPCell(new Phrase("Größe: " + item.size));
-        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        cell.setColspan(5 - (item.donation ? 1 : 0));
-        cell.setBorderColor(BaseColor.WHITE);
-        return cell;
-    }
-
-    private PdfPCell createDescriptionCell(Item item, PdfPTable table) {
-        PdfPCell cell = new PdfPCell(new Phrase(new Chunk(item.description,
-                FontFactory.getFont(FontFactory.HELVETICA, 10))));
-        cell.setColspan(table.getNumberOfColumns() - (item.donation ? 1 : 0));
-        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        cell.setBorderColor(BaseColor.WHITE);
-        cell.setBorder(NUMBER_OF_COLUMNS);
-        return cell;
-    }
-
-    private PdfPCell createCategoryCell(Item item) {
-        PdfPCell cell = new PdfPCell(new Phrase(new Chunk(item.category.name,
-                FontFactory.getFont(FontFactory.HELVETICA, 10))));
-        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        cell.setVerticalAlignment(Element.ALIGN_CENTER);
-        cell.setColspan(3);
-        cell.setBorderColor(BaseColor.WHITE);
-        cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
-        return cell;
-    }
-
-    private PdfPCell createItemNumberCell(Item item) {
-        PdfPCell cell = new PdfPCell(new Phrase(new Chunk(Integer.toString(item.number),
+    private PdfPCell createDetailsCell(Item item) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(item.category.name);
+        sb.append("\n");
+        sb.append(item.description);
+        if (item.size != null && item.size.length() > 0) {
+            sb.append("\nGröße: ");
+            sb.append(item.size);
+        }
+        PdfPCell cell = new PdfPCell(new Phrase(new Chunk(sb.toString(),
                 FontFactory.getFont(FontFactory.HELVETICA, 12))));
         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        cell.setVerticalAlignment(Element.ALIGN_BASELINE);
-        cell.setBorderColor(BaseColor.WHITE);
-        cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        cell.setBorderColor(BaseColor.BLACK);
+        cell.setBorderWidth(1);
         return cell;
     }
 
-    private PdfPCell createSellerNumberCell(Item item) {
-        PdfPCell cell = new PdfPCell(new Phrase(new Chunk(Integer.toString(item.reservation.number),
-                FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18))));
+    private PdfPCell createNumberCell(Item item) {
+        PdfPCell cell = new PdfPCell(new Phrase(new Chunk(
+                String.format("%d - %d", item.reservation.number, item.number),
+                FontFactory.getFont(FontFactory.HELVETICA_BOLD, 20))));
         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        cell.setVerticalAlignment(Element.ALIGN_TOP);
-        cell.setBorderColor(BaseColor.WHITE);
-        cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        cell.setBorderColor(BaseColor.BLACK);
+        cell.setBorderWidth(1);
         return cell;
     }
 
-    private PdfPCell createTableCell(PdfWriter writer, Item item) {
+    private PdfPCell createTableCell(PdfWriter writer, Item item) throws IOException, DocumentException {
         PdfPCell cell = new PdfPCell(new Paragraph(""));
-        cell.setBorderColor(BaseColor.WHITE);
+        cell.setBorder(0);
         cell.addElement(createCellContent(writer, item));
+        cell.setFixedHeight(getLabelCellHeight(writer));
         return cell;
     }
 
-    private PdfPTable createTableLine() {
+    private float getLabelCellHeight(PdfWriter writer) {
+        return (writer.getPageSize().getHeight() - Utilities.millimetersToPoints(45)) / NUMBER_OF_ROWS;
+    }
+
+    private PdfPTable createTable() {
         PdfPTable table = new PdfPTable(NUMBER_OF_COLUMNS);
         table.setWidthPercentage(100);
         return table;
